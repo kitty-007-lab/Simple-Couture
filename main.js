56df4e3ba818a292f056c332f8f2eb0bf97f6ab5
kitty-007-lab <58553216+kitty-007-lab@users.noreply.github.com>
2020-02-13 22:12:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/**slider 1*/
+$(".slider-one").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    prevArrow:".site-slider .slider-btn .prev" ,
+    nextArrow:".site-slider .slider-btn .next",
+  });
